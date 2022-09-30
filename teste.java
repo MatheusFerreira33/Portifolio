fa90a0367d5ef7c55a963130df55b6b4bc61c33a
MatheusFerreira33 <matheus.fsilva33@gmail.com>
2022-09-30 14:22:17 -0300

    teste

--- teste.java
+++ teste.java
@@ -4,6 +4,18 @@ public class teste{
 
     public static void main(String [] args){
         System.out.println("Hello Word");
+
+        teste t = new teste("Matehus");
+
+        
+
+
+    }
+
+    teste(String nome){
+
+        
+
     }
 
 
